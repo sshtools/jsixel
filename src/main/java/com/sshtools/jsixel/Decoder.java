d5a957ef4e44dfe5c4306d93842ab44133c5ac27
Brett Smith <brett@logonbox.com>
2023-06-26 10:30:52 +0100

    Bit of work on Decoder

--- src/main/java/com/sshtools/jsixel/Decoder.java
+++ src/main/java/com/sshtools/jsixel/Decoder.java
@@ -2,7 +2,6 @@ package com.sshtools.jsixel;
 
 import java.io.Closeable;
 import java.io.File;
-import java.nio.ByteBuffer;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -13,59 +12,56 @@ import com.sun.jna.ptr.PointerByReference;
 
 public class Decoder implements Closeable {
 	
-	private final PointerByReference enc;
+	private final PointerByReference dec;
 	private final Set<Memory> allocated = new HashSet<>();
 
 	public Decoder() {
-		enc = new PointerByReference();
-		SixelLibrary.INSTANCE.sixel_encoder_new(enc, (PointerByReference)null);
+		dec = new PointerByReference();
+		SixelLibrary.INSTANCE.sixel_decoder_new(dec, (PointerByReference)null);
 	}
 		
 	public void opt(char flag) {
-		SixelLibrary.INSTANCE.sixel_encoder_setopt(enc, flag, (Pointer)null);
+		SixelLibrary.INSTANCE.sixel_decoder_setopt(dec, flag, (Pointer)null);
 	}
 	
 	public void opt(char flag, String arg) {
-		SixelLibrary.INSTANCE.sixel_encoder_setopt(enc, flag, arg);
+		SixelLibrary.INSTANCE.sixel_decoder_setopt(dec, flag, arg);
 	}
 	
 	public void opt(char flag, int arg) {
 		var memo = new Memory(4);
 		allocated.add(memo);
 		memo.setInt(0, arg);
-		SixelLibrary.INSTANCE.sixel_encoder_setopt(enc, flag, memo);
+		SixelLibrary.INSTANCE.sixel_decoder_setopt(dec, flag, memo);
 	}
 	
 	public void opt(char flag, boolean arg) {
 		var memo = new Memory(1);
 		allocated.add(memo);
 		memo.setInt(0, arg ? 1 : 0);
-		SixelLibrary.INSTANCE.sixel_encoder_setopt(enc, flag, memo);
+		SixelLibrary.INSTANCE.sixel_decoder_setopt(dec, flag, memo);
 	}
 	
 	public void opt(char flag, long arg) {
 		var memo = new Memory(8);
 		allocated.add(memo);
 		memo.setLong(0, arg);
-		SixelLibrary.INSTANCE.sixel_encoder_setopt(enc, flag, memo);
+		SixelLibrary.INSTANCE.sixel_decoder_setopt(dec, flag, memo);
 	}
 	
-	public void encode(File file) {
-		encode(file.getAbsolutePath());
+	public void decode(File file) {
+		decode(file.getAbsolutePath());
 	}
 	
-	public void encode(String file) {
-		SixelLibrary.INSTANCE.sixel_encoder_encode(enc, file);
+	public void decode(String file) {
+		opt(SixelLibrary.SIXEL_OPTFLAG_INPUT, file);
+		SixelLibrary.INSTANCE.sixel_decoder_decode(dec);
 	}
 	
-	public void encodeBytes(ByteBuffer buf, int width, int height, int pixelFormat, byte[] palette, int ncolors) {
-		SixelLibrary.INSTANCE.sixel_encoder_encode_bytes(enc, buf, width, height, pixelFormat, palette == null ? null : ByteBuffer.wrap(palette), ncolors);
-	}
-
 	@Override
 	public void close() {
 		allocated.forEach(Memory::close);
-		SixelLibrary.INSTANCE.sixel_encoder_unref(enc);
+		SixelLibrary.INSTANCE.sixel_decoder_unref(dec);
 	}
 
 }
